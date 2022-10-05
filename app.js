38b2cd0c48dd0cf2fcd68f97ead7302229ac1172
AbhigyanBafna <abhigyan.bafna215001@tsecol.onmicrosoft.com>
2022-10-05 11:07:16 +0530

    Navbar Updates
    Much much more responsive :)

--- app.js
+++ app.js
@@ -4,11 +4,21 @@ const navSlide = () => {
     const navLink = document.querySelectorAll('.navList li');
 
     burger.addEventListener('click', () =>{
+        //Nav Toggle
         nav.classList.toggle('nav-active');
-    });
 
-    navLink.forEach((link,index)=> {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
+        //Animation Nav
+        navLink.forEach((link,index)=> {
+            if(link.style.animation){
+                link.style.animation = ''
+            }
+            else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.4}s`;
+            }
+        });
+
+        //Burger Animation
+        burger.classList.toggle('toggle');
     });
 }
 
