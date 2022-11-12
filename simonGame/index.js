ca043f5864aaa56f5a62437b039f6379c09a7aa5
AbhigyanBafna <abhigyan.bafna215001@tsecol.onmicrosoft.com>
2022-11-12 17:52:37 +0530

    Update index.js

--- simonGame/index.js
+++ simonGame/index.js
@@ -7,9 +7,9 @@ let started = false;
 
 $(document).keypress(function(e){
     if (!started) {
-        $("#level-title").text("Level " + level);
-        nextSequence();
-        started = true;
+
+        nextSequence(); //Starts the game on Keypress
+        started = true; //Set to true so that on another keypress, game doesn't glitch.
     }
 });
 
@@ -25,12 +25,12 @@ $(".btn").on("click",function(e){
 
 function checkAns(currentLevel){
 
-    if(gamePattern[currentLevel] == userPattern[currentLevel]){ 
+    if(gamePattern[currentLevel] == userPattern[currentLevel]){  //To check if the most recent answer is correct.
 
-        if (userPattern.length === gamePattern.length){ //Checks if there are same number of entries in both arrays
+        if (userPattern.length === gamePattern.length){ //Checks for no of elements in both arrays which should be same
 
         setTimeout(function () {
-            nextSequence();
+            nextSequence(); //Calls for next pattern after a 1 second delay
           }, 1000);
 
         }
@@ -42,40 +42,40 @@ function checkAns(currentLevel){
         $("#level-title").text("Game Over, Press Any Key to Restart");
 
         setTimeout(function () {
-        $("body").removeClass("game-over");
+        $("body").removeClass("game-over"); //Ends the game and flashes a red screen with a sound for 0.2 seconds.
         }, 200);
 
-        startOver();
+        startOver(); //Resets everything.
 
     }
 }
 
 function nextSequence(){
 
-    userPattern = [];
+    userPattern = []; //Clears userPattern so that user enters the whole pattern again
     level++;
 
-    $("#level-title").html("Level " + level);
+    $("#level-title").html("Level " + level); //Upgrades to next level
 
     let rand = Math.floor(Math.random()*4);
-    gamePattern.push(bColors[rand]);
+    gamePattern.push(bColors[rand]); //Pushes another random color to the pattern
 
     fButton(bColors[rand]);
-    sPlayer(bColors[rand]);
+    sPlayer(bColors[rand]); // Displays the newly added color to user.
 
 }
 
 function fButton(color){
-    $("#"+color).fadeOut(50).fadeIn(50).fadeOut(50).fadeIn(50);
+    $("#"+color).fadeOut(50).fadeIn(50).fadeOut(50).fadeIn(50); //Flashes the button
 }
 
 function sPlayer(color){
-    var audio = new Audio("sounds/" + color + ".mp3");
+    var audio = new Audio("sounds/" + color + ".mp3"); //Plays required audio
     audio.play();
 }
 
-function startOver() {
+function startOver() { //Resets the level and game pattern.
     level = 0;
     gamePattern = [];
-    started = false;
+    started = false; //Set to false to reset the keypress detection.
   }
